976ab07ab80c7f8e65ac00819d69e0896ed0305c
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-03-20 02:50:36 +0530

    Add files via upload

--- A power N/src/Main.java
+++ A power N/src/Main.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 public class Main {
 
-    public static long power(long A, long N){
-        if (N == 0){ return 1;}
-        if (N %2 ==0){ return power(A, N/2)*power(A, N/2);}
-        else {return  power(A, N/2)*power(A, N/2)*A;}
+    public static long power(long A, long N) {
+        if (N == 0) {
+            return 1;
+        }
+        long temp = power(A, N / 2);
+        if (N % 2 == 0) {
+            return temp * temp;
+        } else {
+            return temp * temp * A;
+        }
     }
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
